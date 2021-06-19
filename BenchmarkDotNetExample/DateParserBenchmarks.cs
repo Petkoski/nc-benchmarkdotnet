59e6556ded73e44b259272b1e212e32a153f8624
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-06-19 12:02:05 +0200

    Add project files.

---
+++ BenchmarkDotNetExample/DateParserBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkDotNetExample
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class DateParserBenchmarks
+    {
+        //Z means UTC time
+        private const string dateTime = "2020-12-13T16:33:06Z";
+        private static readonly DateParser dateParser = new();
+
+        //Baseline benchmark, where it all started (after that we will be optimizing)
+        [Benchmark(Baseline = true)]
+        public void GetYearFromDateTime()
+        {
+            dateParser.GetYearFromDateTime(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSplit()
+        {
+            dateParser.GetYearFromSplit(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSubstring()
+        {
+            dateParser.GetYearFromSubstring(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpan()
+        {
+            dateParser.GetYearFromSpan(dateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpanWithManualConversion()
+        {
+            dateParser.GetYearFromSpanWithManualConversion(dateTime);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Commands
+- dotnet build -c Release //Release dlls are optimized and perform better
+- dotnet [path]\BenchmarkDotNetExample\bin\Release\net5.0\BenchmarkDotNetExample.dll
